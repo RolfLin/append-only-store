94d8fab5ab0cf02815600c933d1ea29e82275cfa
RolfLin <13729042678@163.com>
2018-04-13 20:11:31 +0800

    restful move to topology package

--- topology/src/main/java/indexingTopology/metadata/HDFSBoltConfig.java
+++ topology/src/main/java/indexingTopology/metadata/HDFSBoltConfig.java
@@ -7,7 +7,7 @@ public class HDFSBoltConfig {
      * is used for reconstruction.
      */
     public static boolean RECONSTRUCT = true;
-    public static final String HDFS_LOCAL_HOST = "hdfs://localhost:9000";
-    public static final String HDFS_META_LOG_RELATIVE_PATH = "/user/john/logging/";
+    public static final String HDFS_LOCAL_HOST = "hdfs://localhost:8866";
+    public static final String HDFS_META_LOG_RELATIVE_PATH = "/user/billlin/data/";
     public static final String HDFS_META_LOG_PATH = HDFS_LOCAL_HOST + HDFS_META_LOG_RELATIVE_PATH;
 }
---
+++ topology/src/main/java/indexingTopology/util/server/rest/AdminMessage.java
@@ -0,0 +1,71 @@
+package indexingTopology.util.server.rest;//package server.rest;
+
+
+
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import indexingTopology.util.track.PosSpacialSearchWs;
+
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("admin_message")
+public class AdminMessage {
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes(MediaType.TEXT_PLAIN)
+    public String setWainingMessage(
+            @FormParam("message") String message) {
+        System.out.println(message);
+        return "Success!";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getWarningMessage(@DefaultValue("null") @QueryParam("message") String message) {
+//        String str = message;
+//        str = str.replace(']','}');
+        StringBuilder sb = new StringBuilder(message);
+        sb.replace(0,1,"{");
+        sb.replace(message.length()-1,message.length(),"}");
+        String sbStr = sb.toString();
+        sbStr = sbStr.replace('(','{');
+        sbStr = sbStr.replace(')','}');
+
+
+        System.out.println(sbStr);
+//        TrackSpacialSearchWs trackSpacialSearch = new TrackSpacialSearchWs();
+//        String result = trackSpacialSearch.services(null,str);
+
+//        TrackNew trackNew = new TrackNew();
+//        String result = trackNew.service(null,str);
+
+//        TrackSearchWs trackSearchWs = new TrackSearchWs();
+//        String result = trackSearchWs.services(null,str);
+
+
+
+        PosSpacialSearchWs posSpacialSearchWs = new PosSpacialSearchWs();
+        String result = posSpacialSearchWs.service(null, sbStr,1L,1L,null);
+//        System.out.println(str);
+//        if (message.equals("null")) {
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.put("message", NaiveQueryImpl.instance().getAdminMessage());
+//            return jsonObject.toString();
+//        } else
+//        NaiveQueryImpl.instance().setAdminMessage(message);
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.put("response", result);
+//        System.out.println(result);
+        JSONObject jsonObject = JSONObject.parseObject(result);
+
+
+        JsonObject returnData = new JsonParser().parse(result).getAsJsonObject();
+        return result;
+//        }
+    }
+}
